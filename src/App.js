aea40f076d8bebb5066b52ee7afdc7cf49fd94ae
Mjenius <157293500+MJenius@users.noreply.github.com>
2024-10-25 11:36:14 +0530

    Updated App.js For Only 4 Buttons

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Delivery from './pages/Delivery';
+import Printout from './pages/Printout';
+import Cart from './pages/Cart';
+import OrderTracking from './pages/OrderTracking';
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/delivery" element={<Delivery />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/track-order" element={<OrderTracking />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
